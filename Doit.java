aed55365877c3c0f939a96728ba9016db8b37709
wallaceandzhao <32485302+wallaceandzhao@users.noreply.github.com>
2018-04-17 10:59:56 +0800

    Add files via upload

---
+++ Doit.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+ class Rectangle{
+	private point p1 = new point(); 
+	private point p2 = new point();
+	private point p3 = new point();
+	private point p4 = new point();
+	public Rectangle(point p1,point p2){
+	}
+	public point getP1() {
+		return p1;
+	}
+	public void setP1(double x,double y) {
+		this.p1.x = x;
+		this.p1.y = y;
+	}
+	public void setP2(double x,double y) {
+		this.p2.x = x;
+		this.p2.y = y;
+	}
+	public Rectangle(point p1,point p2,point p3,point p4){
+		
+	}
+	double length() {
+		p1=this.p1;
+		p2=this.p2;
+		p3=this.p3;
+		p4=this.p4;
+		return 2*(p1.x-p2.x)+2*(p1.y-p2.y);
+	}
+	double area(point p1,point p2,point p3,point p4) {
+		p1=this.p1;
+		p2=this.p2;
+		p3=this.p3;
+		p4=this.p4;
+		return (p3.x-p1.x)*(p1.y-p4.y);
+	}
+}
+class point{
+	double x,y;
+ point(){
+	}
+}
+public class Doit{
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+		point p1 = null,p2 = null;
+		Rectangle rec = new Rectangle(p1,p2);
+		rec.setP1(2.1, 3.2);
+		System.out.println(rec.length());
+	}
+}
+
